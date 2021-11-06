e82c2bcd65f7e2eeb0978bc23427def0773b896e
Taylor Bazouzi <taylor.bazouzi@dcmail.ca>
2021-11-05 23:15:40 -0400

    Initial Set Up

---
+++ src/ca/durhamcollege/Main.java
@@ -0,0 +1,11 @@
+package ca.durhamcollege;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello, World");
+
+    }
+
+}
